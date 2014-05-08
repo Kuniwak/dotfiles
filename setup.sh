bc1ed0768adacb68af696486b024c9fcc4fe5043
Kuniwak <orga.chem.job@gmail.com>
2014-05-08 15:04:05 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,39 @@
-#! /usr/bin/bash
-DOTFILES_DIR=`dirname ${0}`
+#!/bin/sh
+# Thnaks, kaorimatz!
 
-if [[ -f ~/.bashrc ]]; then
-    echo $DOTFILES_DIR/.bashrc >> $HOME/.bashrc
+set -e
+set -u
+
+dotfiles=$HOME/.dotfiles
+
+has() {
+  type "$1" > /dev/null 2>&1
+}
+
+symlink() {
+  [ -e "$2" ] || ln -s "$1" "$2"
+}
+
+if [ -d "$dotfiles" ]; then
+  (cd "$dotfiles" && git pull --rebase)
 else
-    ln -s $DOTFILES_DIR/.bashrc $HOME/.bashrc
-fi
-source ${HOME}/.bashrc
-
-if [[ ! -f ~/.gitignore_global ]]; then
-    ln -s $DOTFILES_DIR/.gitignore_global $HOME/.gitignore_global
+  git clone https://github.com/OrgaChem/.dotfiles "$dotfiles"
 fi
 
-if [[ ! -f ~/.gitconfig ]]; then
-    ln -s $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
+has git && symlink "$dotfiles/.gitconfig" "$HOME/.gitconfig"
+has git && symlink "$dotfiles/.gitignore_global" "$HOME/.gitignore_global"
+
+if has vim; then
+  vimfiles=$HOME/.orgachem-vim
+  git clone https://github.com/OrgaChem/OrgaChem-Vim-Configurations "$vimfiles"
+  symlink "$vimfiles/_vimrc" "$HOME/.vimrc"
+  symlink "$vimfiles/_gvimrc" "$HOME/.gvimrc"
+  symlink "$vimfiles/vimfiles" "$HOME/.vim"
+  git clone https://github.com/Shougo/neobundle.vim "$vimfiles/bundle/neobundle.vim"
 fi
+
+if [ -f "$HOME/.bashrc" ]; then
+  mv "$HOME/.bashrc" "$HOME/.bashrc.org" 
+fi
+
+has git && symlink "$dotfiles/.bashrc" "$HOME/.bashrc"
