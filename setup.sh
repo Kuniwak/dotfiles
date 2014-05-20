88d326b0e0e19623e6f36ff67e6bb9836bef0572
Kuniwak <yuki.kokubun@mixi.co.jp>
2014-05-20 11:16:40 +0900

    Merge vim settings

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Thnaks, kaorimatz!
+# Thanks, @kaorimatz!
 
 set -e
 set -u
@@ -18,28 +18,25 @@ setup() {
 	if [ -d "$dotfiles" ]; then
 		(cd "$dotfiles" && git pull --rebase)
 	else
-		git clone https://github.com/OrgaChem/.dotfiles "$dotfiles"
+		git clone https://github.com/Kuniwak/.dotfiles "$dotfiles"
 	fi
 
 	has git && symlink "$dotfiles/.gitconfig" "$HOME/.gitconfig"
 	has git && symlink "$dotfiles/.gitignore.global" "$HOME/.gitignore.global"
 	has curl && symlink "$dotfiles/.curlrc" "$HOME/.curlrc"
 	has tig && symlink "$dotfiles/.tigrc" "$HOME/.tigrc"
-	has git && symlink "$dotfiles/.zshrc" "$HOME/.zshrc"
-	has git && symlink "$dotfiles/.zshenv" "$HOME/.zshenv"
-	has git && symlink "$dotfiles/.zshprofile" "$HOME/.zshprofile"
+
+	if has zsh; then
+		symlink "$dotfiles/.zshrc" "$HOME/.zshrc"
+		symlink "$dotfiles/.zshenv" "$HOME/.zshenv"
+		symlink "$dotfiles/.zshprofile" "$HOME/.zshprofile"
+	fi
 
 	if has vim; then
-		vimfiles=$HOME/.orgachem-vim
-		if [ -d "$vimfiles" ]; then
-			(cd "$vimfiles" && git pull --rebase)
-		else
-			git clone https://github.com/OrgaChem/OrgaChem-Vim-Configurations "$vimfiles"
-			symlink "$vimfiles/_vimrc" "$HOME/.vimrc"
-			symlink "$vimfiles/_gvimrc" "$HOME/.gvimrc"
-			symlink "$vimfiles/vimfiles" "$HOME/.vim"
-			git clone https://github.com/Shougo/neobundle.vim "$HOME/.vim/bundle/neobundle.vim"
-		fi
+		symlink "$vimfiles/.vimrc" "$HOME/.vimrc"
+		symlink "$vimfiles/.gvimrc" "$HOME/.gvimrc"
+		symlink "$vimfiles/vimfiles" "$HOME/.vim"
+		git clone https://github.com/Shougo/neobundle.vim "$HOME/.vim/bundle/neobundle.vim"
 	fi
 }
 
