bc4522b48631737895bfc1a4c094a8c7a1103f44
Kuniwak <yuki.kokubun@mixi.co.jp>
2016-06-02 12:47:59 +0900

    NODE POWER

--- setup-js.sh
+++ setup-js.sh
@@ -7,7 +7,7 @@ has() {
 }
 
 setup_ndenv() {
-	RECOMMENDED_NODE_VAR="v0.10.36"
+	RECOMMENDED_NODE_VAR="v5.11.1"
 	git clone git://github.com/riywo/ndenv.git "$HOME/.ndenv"
 	git clone git://github.com/riywo/node-build.git "$HOME/.ndenv/plugins/node-build/"
 
@@ -18,21 +18,35 @@ setup_ndenv() {
 	ndenv global $RECOMMENDED_NODE_VAR
 }
 
-setup_jshint() {
+setup_lints() {
 	has ndenv || setup_ndenv
-	npm install -g jshint
+	npm install -g jshint eslint jsonlint
+	ndenv rehash
+}
+
+setup_debug_tools() {
+	has ndenv || setup_ndenv
+	npm install -g js-beautify css-semdiff
+	ndenv rehash
+}
+
+setup_typescript() {
+	has ndenv || setup_ndenv
+	npm install -g tslint typescript
 	ndenv rehash
 }
 
 setup_gulp() {
 	has ndenv || setup_ndenv
-	npm install -g gulp
+	npm install -g gulp-cli
 	ndenv rehash
 }
 
 setup_node() {
-	has jshint || setup_jshint
-	has gulp || setup_gulp
+	setup_lints
+	setup_debug_tools
+	setup_gulp
+	setup_typescript
 }
 
 setup_node
