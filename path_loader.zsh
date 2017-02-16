cfaed8681b88ec9081660acd6c02bb357d2b4283
Kuniwak <orga.chem.job@gmail.com>
2017-02-16 23:57:29 +0900

    BETTER SH

--- path_loader.zsh
+++ path_loader.zsh
@@ -1,25 +1,25 @@
 #! /bin/zsh
 
 MY_RBENV="$HOME/.rbenv"
-if [ -d $MY_RBENV ]; then
+if [[ -d $MY_RBENV ]]; then
 	export PATH="$MY_RBENV/bin:$MY_RBENV/shims:$PATH"
 	eval "$(rbenv init -)"
 fi
 
 MY_PLENV="$HOME/.plenv"
-if [ -d $MY_PLENV ]; then
+if [[ -d $MY_PLENV ]]; then
 	export PATH="$MY_PLENV/bin:$MY_PLENV/shims:$PATH"
 	eval "$(plenv init -)"
 fi
 
 MY_NDENV="$HOME/.ndenv"
-if [ -d $MY_NDENV ]; then
+if [[ -d $MY_NDENV ]]; then
 	export PATH="$MY_NDENV/bin:$MY_NDENV/shims:$PATH"
 	eval "$(ndenv init -)"
 fi
 
 MY_DENV="$HOME/.denv"
-if [ -d $MY_DENV ]; then
+if [[ -d $MY_DENV ]]; then
 	export PATH="$MY_DENV/bin:$MY_DENV/shims:$PATH"
 	eval "$(denv init -)"
 fi
@@ -29,12 +29,12 @@ if has npm; then
 fi
 
 MY_PYENV="$HOME/.pyenv"
-if [ -d $MY_PYENV ]; then
+if [[ -d $MY_PYENV ]]; then
 	export PYENV_ROOT=$MY_PYENV
 	export PATH="$MY_PYENV/bin:$MY_PYENV/shims:$PATH"
 	eval "$(pyenv init -)"
 
-	if [ -d "$MY_PYENV/plugins/pyenv-virtualenv" ];then
+	if [[ -d "$MY_PYENV/plugins/pyenv-virtualenv" ]];then
 		eval "$(pyenv virtualenv-init -)"
 	fi
 fi
@@ -47,7 +47,7 @@ if [[ -d "$MAC_PYTHON_DIR" ]]; then
 fi
 
 SOCK="/tmp/ssh-agent-$USER"
-if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $SOCK ]
+if test $SSH_AUTH_SOCK && [[ $SSH_AUTH_SOCK != $SOCK ]]
 then
     rm -f $SOCK
     ln -sf $SSH_AUTH_SOCK $SOCK
@@ -55,12 +55,12 @@ then
 fi
 
 MY_CARGO="$HOME/.cargo"
-if [ -d $MY_CARGO ]; then
+if [[ -d $MY_CARGO ]]; then
 	export PATH="$MY_CARGO/bin:$PATH"
 fi
 
 MY_SWIFTENV="$HOME/.swiftenv"
-if [ -d $MY_SWIFTENV ]; then
+if [[ -d $MY_SWIFTENV ]]; then
 	export SWIFTENV_ROOT="$MY_SWIFTENV"
 	export PATH="$MY_SWIFTENV/bin:$PATH"
 	eval "$(swiftenv init -)"
@@ -68,6 +68,6 @@ fi
 
 
 JAVA_HOME_DETECTOR="/usr/libexec/java_home"
-if [ -x "$JAVA_HOME_DETECTOR" ]; then
+if [[ -x "$JAVA_HOME_DETECTOR" ]]; then
 	export JAVA_HOME="$($JAVA_HOME_DETECTOR)"
 fi
