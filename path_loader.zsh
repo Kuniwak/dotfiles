648c9d07c5e3075d8bdc62798ec692b7778f0002
Kuniwak <orga.chem.job@gmail.com>
2017-02-16 16:16:11 +0900

    pyenv 離れが加速

--- path_loader.zsh
+++ path_loader.zsh
@@ -39,6 +39,10 @@ if [ -d $MY_PYENV ]; then
 	fi
 fi
 
+for pip_dir in $(find "$HOME/Library/Python" -type d -depth 1); do
+	export PATH="$pip_dir/bin:$PATH"
+done
+
 SOCK="/tmp/ssh-agent-$USER"
 if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $SOCK ]
 then
