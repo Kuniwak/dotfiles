14238f09420ad8516449b40508ed17595331b7f5
Kuniwak <orga.chem.job@gmail.com>
2018-04-17 19:43:43 +0900

    IROIRO POWER

---
+++ setup-go.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -eu
+
+has() {
+	type "$1" > /dev/null 2>&1
+}
+
+setup_goenv() {
+	git clone git://github.com/syndbg/goenv.git "$HOME/.goenv"
+
+	export PATH="$HOME/.goenv/bin:$PATH"
+	eval "$(goenv init -)"
+}
+
+setup_goenv
---
+++ setup-tmux.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -eu
+set -o pipefail
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
