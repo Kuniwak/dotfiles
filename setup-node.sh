afa0d39d8a15a050810e200f01908c517777f029
Kuniwak <orga.chem.job@gmail.com>
2020-10-12 11:41:37 +0900

    Upgrade node version

--- setup-node.sh
+++ setup-node.sh
@@ -17,7 +17,7 @@ install_nodenv() {
 }
 
 setup_nodenv() {
-	RECOMMENDED_NODE_VAR=12.16.1
+	RECOMMENDED_NODE_VAR=14.11.0
 
 	nodenv install --skip-existing "$RECOMMENDED_NODE_VAR"
 	nodenv global "$RECOMMENDED_NODE_VAR"
